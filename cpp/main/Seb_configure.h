80e43e9e93337e4701ddcbd3aa737b95fc327b06
Adithi Narayan <adithin2001@gmail.com>
2024-10-22 19:38:13 -0400

    Miniball for windows

--- cpp/main/Seb_configure.h
+++ cpp/main/Seb_configure.h
@@ -9,27 +9,15 @@
 #include <cassert>
 #include <iomanip>
 #include <iostream>
+#include <sstream> // Ensure this is included
 
-#define SEB_NAMESPACE Seb              // namespace of the library
-// #define SEB_ASSERTION_MODE          // enables (cheap) assertions
-// #define SEB_DEBUG_MODE              // enables debugging checks (cheap and
-                                       // expensive ones) this enables
-                                       // assertion and timer mode as well
-// #define SEB_TIMER_MODE              // enables runtime measurements
-// #define SEB_STATS_MODE              // enables statistics
+#define SEB_NAMESPACE Seb // namespace of the library
+// #define SEB_ASSERTION_MODE // enables (cheap) assertions
+// #define SEB_DEBUG_MODE // enables debugging checks (cheap and expensive ones)
+// #define SEB_TIMER_MODE // enables runtime measurements
+// #define SEB_STATS_MODE // enables statistics
 
 #include "Seb_debug.h"
-
-// Fixes for GCC series 2.95.  (This fix is necessary for 2.95.2 at
-// least.  But I guess it is needed for any earlier version of the 2.95
-// series, too.)  Apparently, GCC doesn't come with a bitset and sstream
-// implementation, that's why we include them here.
-#if defined(__GNUC__) && __GNUC__==2 && \
-  __GNUC_MINOR__==95 && __GNUC_PATCHLEVEL__ <= 2
-  #include <gcc2-95-2_fix.h>
-#else
-  #include <sstream>
-#endif
 
 // We provide the following debugging routines:
 //
@@ -37,22 +25,21 @@
 //   is on (see SEB_DEBUG_MODE above), or does nothing otherwise.
 //
 // - SEB_LOG(channel,expr): Writes to file channel.log the string
-//   produced by the expression 's << expr' (where s it the stream for
-//   file channel.log).  This only happens when the debugging mode is
+//   produced by the expression 's << expr' (where s is the stream for
+//   file channel.log). This only happens when the debugging mode is
 //   enabled; nothing is done otherwise.
-//   Example: SEB_LOG("debug","Radius is " << radius_square << std::endl)
 //
 // - SEB_ASSERT(expr): Asserts that the Boolean expression expr evaluates
-//   to true.  This only happens when the assertion mode is enabled.  This
+//   to true. This only happens when the assertion mode is enabled. This
 //   macro should only be used for cheap assertions (which do not heavily
 //   affect the running time).
 //
 // - SEB_ASSERT_EXPENSIVE(expr): Asserts that the Boolean expression expr
-//   evaluates to true.  This only happens when the debugging mode is on,
-//   and not otherwise.  (The assertion mode alone doesn't enable such
+//   evaluates to true. This only happens when the debugging mode is on,
+//   and not otherwise. (The assertion mode alone doesn't enable such
 //   assertions.)
 //
-// - SEB_TIMER_START(timer): Restarts the timer with name timer.  Nothing
+// - SEB_TIMER_START(timer): Restarts the timer with name timer. Nothing
 //   happens when the timer mode is disabled (see SEB_TIMER_MODE above).
 //
 // - SEB_TIMER_PRINT(timer): Prints a string to std::cout telling how
@@ -63,7 +50,6 @@
 //
 // - SEB_STATS(expr): evaluates the expression when stats mode is
 //   enabled (see SEB_STATS_MODE above), or does nothing otherwise.
-//
 
 // Debugging mode:
 #ifdef SEB_DEBUG_MODE
@@ -77,16 +63,16 @@
 
   #define SEB_ASSERT_EXPENSIVE(expr) assert(expr)
   #define SEB_DEBUG(expr) expr
-  #define SEB_LOG(channel,expr) \
+  #define SEB_LOG(channel, expr) \
     { \
       std::stringstream s; \
       s << expr; \
-      SEB_NAMESPACE::Logger::instance().log(channel,s.str()); \
+      SEB_NAMESPACE::Logger::instance().log(channel, s.str()); \
     }
 #else // SEB_DEBUG_MODE not defined
   #define SEB_ASSERT_EXPENSIVE(expr)
   #define SEB_DEBUG(expr)
-  #define SEB_LOG(channel,expr)
+  #define SEB_LOG(channel, expr)
 #endif // SEB_DEBUG_MODE
 
 // Assertion mode:
@@ -111,7 +97,7 @@
     }
   #define SEB_TIMER_STRING(timer) \
     SEB_NAMESPACE::Timer::instance().lapse(timer)
-#else // SEB_TIMER_MOD
+#else // SEB_TIMER_MODE
   #define SEB_TIMER_START(timer)
   #define SEB_TIMER_PRINT(timer)
   #define SEB_TIMER_STRING(timer)
--- cpp/main/Seb_debug.C
+++ cpp/main/Seb_debug.C
@@ -4,8 +4,11 @@
 //          Kaspar Fischer <kf@iaeth.ch>
 
 #include <cmath>
-#include <sys/resource.h>
-#include <sys/time.h>
+#include <fstream>
+#include <map>
+#include <string>
+#include <chrono>
+#include <windows.h>
 
 #include "Seb_configure.h"
 
@@ -13,46 +16,36 @@ namespace SEB_NAMESPACE {
 
   // Implementation of class Logger:
 
-  Logger::Logger()
-  {
+  Logger::Logger() {
   }
 
-  Logger::~Logger()
-  {
-    // we walk through the list of all opened files and close them:
-    for (Streams::iterator it = channels.begin();
-         it != channels.end(); ++it) {
-      (*it).second->close();
-      delete (*it).second;
+  Logger::~Logger() {
+    // We walk through the list of all opened files and close them:
+    for (auto it = channels.begin(); it != channels.end(); ++it) {
+      it->second->close();
+      delete it->second;
     }
   }
 
-  Logger& Logger::instance()
-  {
-    // Here's where we maintain the only instance: (Notice that it
-    // gets constructed automatically the first time instance() is
-    // called, and that it gets disposed of (if ever contructed) at
-    // program termination.)
+  Logger& Logger::instance() {
+    // Here's where we maintain the only instance:
     static Logger instance;
     return instance;
   }
 
-  void Logger::log(const char* ch,const std::string& msg)
-  {
+  void Logger::log(const char* ch, const std::string& msg) {
     const std::string name(ch);
-    Streams::iterator it = channels.find(name);
+    auto it = channels.find(name);
 
-    // have we already opened this file?
+    // Have we already opened this file?
     if (it != channels.end()) {
       // If so, then just append the message:
-      *(*it).second << msg;
-      (*it).second->flush();
-
+      *(it->second) << msg;
+      it->second->flush();
     } else {
       // If we haven't seen 'name' before, we create a new file:
-      using std::ofstream;
-      ofstream *o = new ofstream((name+".log").c_str(),
-                                 ofstream::out|ofstream::trunc);
+      std::ofstream* o = new std::ofstream((name + ".log").c_str(),
+                                            std::ofstream::out | std::ofstream::trunc);
       channels[name] = o;
       *o << msg;
     }
@@ -60,64 +53,40 @@ namespace SEB_NAMESPACE {
 
   // Implementation of class Timer:
 
-  // The following routine is taken from file mptimeval.h from
-  // "Matpack Library Release 1.7.1" which is copyright (C) 1991-2002
-  // by Berndt M. Gammel.  It works on the timeval struct defined in
-  // sys/time.h:
-  //
-  //       struct timeval {
-  //         long tv_sec;        /* seconds */
-  //         long tv_usec;       /* microseconds */
-  //       };
-  //
-  inline timeval& operator-=(timeval &t1,const timeval &t2)
-  {
-    t1.tv_sec -= t2.tv_sec;
-    if ( (t1.tv_usec -= t2.tv_usec) < 0 ) {
-      --t1.tv_sec;
-      t1.tv_usec += 1000000;
-    }
-    return t1;
+  Timer::Timer() {
   }
 
-  Timer::Timer()
-  {
-  }
-
-  Timer& Timer::instance()
-  {
-    // Here's where we maintain the only instance: (Notice that it
-    // gets constructed automatically the first time instance() is
-    // called, and that it gets disposed of (if ever contructed) at
-    // program termination.)
+  Timer& Timer::instance() {
+    // Here's where we maintain the only instance:
     static Timer instance;
     return instance;
   }
 
-  void Timer::start(const char *timer_name)
-  {
-    // fetch current usage:
-    rusage now;
-    int status = getrusage(RUSAGE_SELF,&now);
-    SEB_ASSERT(status == 0);
-
-    // save it:
-    timers[std::string(timer_name)] = now.ru_utime;
+  void Timer::start(const char *timer_name) {
+    // Get current time:
+    LARGE_INTEGER now;
+    QueryPerformanceCounter(&now);
+    timers[std::string(timer_name)] = now.QuadPart;
   }
 
-  float Timer::lapse(const char *name)
-  {
-    // assert that start(name) has been called before:
+  float Timer::lapse(const char *name) {
+    // Assert that start(name) has been called before:
     SEB_ASSERT(timers.find(std::string(name)) != timers.end());
 
-    // get current usage:
-    rusage now;
-    int status = getrusage(RUSAGE_SELF,&now);
-    SEB_ASSERT(status == 0);
+    // Get current time:
+    LARGE_INTEGER now;
+    QueryPerformanceCounter(&now);
 
-    // compute elapsed usage:
-    now.ru_utime -= (*timers.find(std::string(name))).second;
-    return now.ru_utime.tv_sec + now.ru_utime.tv_usec * 1e-6;
+    // Compute elapsed time:
+    float elapsed = (now.QuadPart - timers[std::string(name)]) * 1000.0f / frequency.QuadPart;
+    return elapsed; // Return time in milliseconds
   }
+
+  void Timer::initialize() {
+    // Initialize the frequency for high-resolution performance counter
+    QueryPerformanceFrequency(&frequency);
+  }
+
+  LARGE_INTEGER Timer::frequency;
 
 } // namespace SEB_NAMESPACE
